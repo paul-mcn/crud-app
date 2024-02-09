a479c413ef2c6abd677776f506ff48748d212545
paul-mcn <paul.a.mcnamara@outlook.com>
2024-02-09 13:38:02 +1100

    added cors, changed root route to /api

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,20 +1,22 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 const port = 4000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 /**
  * Test api call
  */
-app.get("/ping", (req, res) => {
+app.get("/api/ping", (req, res) => {
 	res.send("pong\n");
 });
 
-app.use("/meals", routes);
+app.use("/api/meals", routes);
 
 app.use((err, req, res, next) => {
 	const statusCode = err.status || 500;
